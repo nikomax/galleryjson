d7f9bf96d47d5ec4b0e5aff2a05e2cc48ef13cd9
Maksym <maks@coderiver.com.ua>
2017-11-03 11:34:15 +0200

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import $ from 'jquery';
 
-
 var xhr = new XMLHttpRequest();
 var imageList = $('.image-list');
 var bigImgCont = $('.big-image');
@@ -10,6 +9,7 @@ xhr.open('GET', 'https://jsonplaceholder.typicode.com/photos', false);
 
 xhr.send();
 
+// Разбивка массива на двумерный массив с длинами size
 function chunk(arr, size) {
   var result = [];
   for (var j = 0; j < arr.length; j += size) {
@@ -24,38 +24,53 @@ if (xhr.status !== 200) {
 
   var data = JSON.parse(xhr.response);
   var arrId = [];
-  for (var i = 0; i < data.length / 100; i++) {
+  for (var i = 0; i < data.length / 10; i++) {
     arrId.push(data[i].id);
   }
   var chunkedArr = chunk(arrId, 5);
 
   var page = 0;
 
+
+  // Вывод списка изображений
   function showList() {
+    // $('.loader-clock').addClass('is-active');
     for (var i = 0; i < chunkedArr[page].length; i++) {
       var smallUrl = getSmallById(chunkedArr[page][i]);
       $(imageList).append(`<li class="image-list__item"><img src="${smallUrl}"></li>`);
+      $('.loader-clock').removeClass('is-active');
     }
   }
 
-
   showList();
 
+  // Следующий список изображений
+  function prevPage() {
+    page -= 1;
+    $(imageList).html('');
+    showList();
+  }
+
+  // Пред. список
+  function nextPage() {
+    page += 1;
+    $(imageList).html('');
+    showList();
+  }
+
+  // Кнопка на предыдущий список
   $('.prev-list').on('click', function(event) {
     event.stopPropagation();
     if (page > 0) {
-      page -= 1;
-      $(imageList).html('');
-      showList();
+      prevPage();
     }
   });
 
+  // Кнопка на след. список
   $('.next-list').on('click', function(event) {
     event.stopPropagation();
     if (page < chunkedArr.length-1) {
-      page += 1;
-      $(imageList).html('');
-      showList();
+      nextPage();
     }
   });
 
@@ -92,28 +107,37 @@ if (xhr.status !== 200) {
     }
   }
 
+  // Просмотр большого изображения при клике на маленькое
   imageList.on('click', function(event) {
     event.preventDefault();
     var target = event.target;
     if (target.tagName !== 'IMG') return;
+    $('.loader-clock').addClass('is-active');
     var imageSrc = $(target).attr('src');
     $(this).find('.image-list__item').removeClass('is-active');
     $(target).parent().addClass('is-active');
     $(this).parents('.gallery').find('.big-image').find(bigImg).attr('src', getBigBySmall(imageSrc));
-
+    $('.loader-clock').removeClass('is-active');
   });
 
+  // Скрыть оверлей
   $(window).click(function() {
     $('.overlay').hide();
   });
 
+
+  // Вывод большого изображения на весь екран
   bigImgCont.on('click', function(event) {
     event.stopPropagation();
+    $('.loader-clock').addClass('is-active');
     $(this).parents('.gallery').find('.overlay').show().find('.preview__img').attr('src', $(this).find(bigImg).attr('src'));
+    $('.loader-clock').removeClass('is-active');
   });
 
+  // Следующее большое изображение
   $('.btn-next').on('click', function(event) {
     event.stopPropagation();
+    $('.loader-clock').addClass('is-active');
     var id = getIdByBig($(this).siblings('.preview__img').attr('src'));
     var nextId = id + 1;
     $(this).siblings('.preview__img').attr('src', getBigById(nextId));
@@ -121,10 +145,17 @@ if (xhr.status !== 200) {
     $(this).parents('.gallery').find('.image-list__item').removeClass('is-active');
     var smallSrc = getSmallById(nextId);
     $(this).parents('.gallery').find('.image-list__item').find(`img[src = "${smallSrc}"]`).parent().addClass('is-active');
+    if (id % 5 === 0) {
+      nextPage();
+      $(this).parents('.gallery').find('.image-list__item').find(`img[src = "${smallSrc}"]`).parent().addClass('is-active');
+    }
+    $('.loader-clock').removeClass('is-active');
   });
 
+  // Пред. большое изображение
   $('.btn-prev').on('click', function(event) {
     event.stopPropagation();
+    $('.loader-clock').addClass('is-active');
     var id = getIdByBig($(this).siblings('.preview__img').attr('src'));
     var prevId = id - 1;
     $(this).siblings('.preview__img').attr('src', getBigById(prevId));
@@ -132,6 +163,11 @@ if (xhr.status !== 200) {
     $(this).parents('.gallery').find('.image-list__item').removeClass('is-active');
     var smallSrc = getSmallById(prevId);
     $(this).parents('.gallery').find('.image-list__item').find(`img[src = "${smallSrc}"]`).parent().addClass('is-active');
+    if (id % 5 === 1) {
+      prevPage();
+      $(this).parents('.gallery').find('.image-list__item').find(`img[src = "${smallSrc}"]`).parent().addClass('is-active');
+    }
+    $('.loader-clock').removeClass('is-active');
   });
 
 }
